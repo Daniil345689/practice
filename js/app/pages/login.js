4f0ae3053a20632c3cb00afd2c815ab62fb344f5
Daniil345689 <danya060806@gmail.com>
2026-01-22 16:43:59 +0200

    Add files via upload

---
+++ js/app/pages/login.js
@@ -0,0 +1,17 @@
+export const login = {
+    data:function() {
+return {
+img: 1,
+hs:0,
+parent:''
+}
+},
+mounted: function(){
+this.img = this.randomIntFromInterval(1,7);
+this.parent = this. Sparent.sparent;
+},
+template:
+<div class="flex">
+gdfgdfgdfgdf
+</div>
+};
