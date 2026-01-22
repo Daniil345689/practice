82cbe28a88292f7eebedfe7fbc35d167e3b0c77e
Daniil345689 <danya060806@gmail.com>
2026-01-22 16:32:14 +0200

    Add files via upload

---
+++ js/app/pages/login.js
@@ -0,0 +1,9 @@
+export const login = {
+    template: `
+        <section class="auth-page">
+            <div class="auth-box">
+                <p class="auth-text">gdfgdfgdf</p>
+            </div>
+        </section>
+    `
+};
