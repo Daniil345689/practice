b6bece692cead4ee5fcfe73b776663f7567ebd3b
Daniil345689 <danya060806@gmail.com>
2026-01-22 16:34:02 +0200

    Delete js/app/pages directory

--- js/app/pages/login.js
+++
@@ -1,9 +0,0 @@
-export const login = {
-    template: `
-        <section class="auth-page">
-            <div class="auth-box">
-                <p class="auth-text">gdfgdfgdf</p>
-            </div>
-        </section>
-    `
-};
