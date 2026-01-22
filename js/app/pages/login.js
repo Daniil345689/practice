6ffac6295663783a94b6fb3289e8699892c245ab
Daniil345689 <danya060806@gmail.com>
2026-01-22 16:30:10 +0200

    Update login.js

--- js/app/pages/login.js
+++ js/app/pages/login.js
@@ -2,9 +2,9 @@ export const login = {
     template: `
         <section class="auth-page">
             <div class="auth-box">
-                <span class="auth-title">Login page</span>
-                <p class="auth-text">Test component</p>
+                <p class="auth-text">gdfgdfgfd</p>
             </div>
         </section>
     `
 };
+
